4abb765a9ef68869db9e2d4f6a4df5f5b17f61e9
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2023-02-18 12:05:52 -0500

    Item Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    subcategory = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return 'Item: [name = {}, category = {}, subcategory = {}, amount = {}]'\
+            .format(self.name, self.category, self.subcategory, self.amount)
