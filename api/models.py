dd4f5721242c2ee028e25953c54cc2851da0c9d2
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2023-02-18 14:18:25 -0500

    Code refactoring

--- api/models.py
+++ api/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     subcategory = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     amount = models.PositiveIntegerField()
 
     def __str__(self):
-        return 'Item: [name = {}, category = {}, subcategory = {}, amount = {}]'\
+        return 'Item: [category = {}, subcategory = {}, name = {}, amount = {}]'\
             .format(self.name, self.category, self.subcategory, self.amount)
