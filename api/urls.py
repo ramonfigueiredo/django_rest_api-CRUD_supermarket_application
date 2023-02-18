1e769fc1b137732a8f34d007e8b6482db50a5c6e
Ramon Figueiredo Pessoa <ramon.fgrd@gmail.com>
2023-02-18 14:27:03 -0500

    View items endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 urlpatterns = [
     path('', views.ApiOverview, name='home'),
-    path('create/', views.add_items, name='add-items')
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view-items'),
 ]
--- api/views.py
+++ api/views.py
@@ -12,7 +12,8 @@ def index(request):
                         "<p><a href='http://127.0.0.1:8000/admin'>Admin page</a></p>"
                         "<p><a href='http://127.0.0.1:8000/api'>API</a></p>"
                         "<ol>"
-                        "<li><a href='http://127.0.0.1:8000/api/create'>Create Item</a></li>"
+                        "<li><a href='http://127.0.0.1:8000/api/create'>Create item</a></li>"
+                        "<li><a href='http://127.0.0.1:8000/api/all'>View items</a></li>"
                         "</ol>")
 
 @api_view(['GET'])
@@ -40,5 +41,21 @@ def add_items(request):
     if item.is_valid():
         item.save()
         return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def view_items(request):
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Item.objects.filter(**request.query_params.dict())
+    else:
+        items = Item.objects.all()
+
+    # if there is something in items else raise error
+    if items:
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
